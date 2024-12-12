6d2cf1de35977ed3300cf90f439c804891b228b4
darkwolf1000 <76166024+darkwolf1000@users.noreply.github.com>
2024-12-12 13:18:37 +0100

    Update targetver.h
    Übersetztung

--- WinKeyboardLocker/targetver.h
+++ WinKeyboardLocker/targetver.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
-// // 包含 SDKDDKVer.h 可定义可用的最高版本的 Windows 平台。
-// 如果希望为之前的 Windows 平台构建应用程序，在包含 SDKDDKVer.h 之前请先包含 WinSDKVer.h 并
-// 将 _WIN32_WINNT 宏设置为想要支持的平台。
+// Fügen Sie SDKDDKVer.h ein, um die höchste verfügbare Version der Windows-Plattform zu definieren.
+// Wenn Sie Anwendungen für frühere Windows-Plattformen erstellen möchten, schließen Sie WinSDKVer.h vor SDKDDKVer.h ein und 
+// setzen Sie das Makro _WIN32_WINNT auf die Plattform, die Sie unterstützen möchten.
+// Setzen Sie das _WIN32_WINNT-Makro auf die Plattform, die Sie unterstützen möchten.
 #include <SDKDDKVer.h>
