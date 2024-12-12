81e34576582089c4c0fe4ab65863e4410ad892de
darkwolf1000 <76166024+darkwolf1000@users.noreply.github.com>
2024-12-12 13:16:12 +0100

    Update Name.h
    Übersetztung

--- WinKeyboardLocker/Name.h
+++ WinKeyboardLocker/Name.h
@@ -1,15 +1,15 @@
 #pragma once
 #define WKBL_APP_NAME  L"Win Keyboard Locker"
 #define WKBL_APP_TITLE  L"Win Keyboard Locker"
-#define WKBL_APP_ERROR L"程序出现错误！"
+#define WKBL_APP_ERROR L"Fehler"
 
-#define WKBL_TRAYICON_HOVER L"右键操作"
+#define WKBL_TRAYICON_HOVER L"Keyboard Locker"
 
-#define WKBL_NOTIFY_START L"程序已启动！\n请在系统托盘里进行操作\n"
-#define WKBL_NOTIFY_UNLOCK L"键盘已解锁!"
-#define WKBL_NOTIFY_LOCK L"键盘已锁定!"
-#define WKBL_NOTIFY_LOCK2 L"请在托盘中操作或点击下方按钮解锁键盘"
+#define WKBL_NOTIFY_START L"Tastatur jetzt sperren"
+#define WKBL_NOTIFY_UNLOCK L"Tastatur ist jetzt entsperrt"
+#define WKBL_NOTIFY_LOCK L"Tastatur ist jetzt gesperrt"
+#define WKBL_NOTIFY_LOCK2 L"Tastatur ist gesperrt hier klicken zum entsperren"
 
-#define WKBL_MENU_LOCK L"锁定键盘"
-#define WKBL_MENU_UNLOCK L"解锁键盘"
-#define WKBL_MENU_EXIT L"退出"
+#define WKBL_MENU_LOCK L"Tastatur Sperren"
+#define WKBL_MENU_UNLOCK L"Tastatur entsperren"
+#define WKBL_MENU_EXIT L"Beenden"
