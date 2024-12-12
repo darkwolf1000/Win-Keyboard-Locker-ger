e03f014762b34e81fa4441fe7f869cd4f846aa89
darkwolf1000 <76166024+darkwolf1000@users.noreply.github.com>
2024-12-12 13:26:17 +0100

    Update framework.h
    Übersetztung

--- WinKeyboardLocker/framework.h
+++ WinKeyboardLocker/framework.h
@@ -1,14 +1,14 @@
-﻿// header.h: 标准系统包含文件的包含文件，
-// 或特定于项目的包含文件
+﻿// header.h: Include-Dateien für standardmäßige System-Include-Dateien, // oder projektspezifische Include-Dateien
+// oder projektspezifische Include-Dateien
 //
 
 #pragma once
 
 #include "targetver.h"
 #define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
-// Windows 头文件
+// Windows Header-Dateien
 #include <windows.h>
-// C 运行时头文件
+// C Laufzeit-Header-Datei
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
